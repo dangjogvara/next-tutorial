1094ba9bb33ec9f73dde24961e2c036b124b1c96
Dan Poulsen <dangjogvara@gmail.com>
2023-01-05 09:34:34 +0000

    progress link

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
