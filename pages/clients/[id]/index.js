084bce34348e54245120adfa9c222fd86818b437
Dan Poulsen <dangjogvara@gmail.com>
2023-01-05 13:42:04 +0000

    module finished

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,16 @@
+import { useRouter } from 'next/router';
+
 export default function ClientProjectPage() {
+  const router = useRouter();
+
+  const LoadProjecthandler = () => {
+    router.push('/clients/1/projecta');
+  };
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={LoadProjecthandler}>Load Project A</button>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,9 @@ export default function HomePage() {
         <li>
           <Link href="/portfolio">Portfolio</Link>
         </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
       </ul>
     </div>
   );
