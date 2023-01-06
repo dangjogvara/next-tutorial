3ead803e7ace36ca8adb5acfc3c15b04f362fc66
Dan Poulsen <dangjogvara@gmail.com>
2023-01-06 10:09:23 +0000

    custom button and css

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,8 @@
 import Image from 'next/image';
-import Link from 'next/link';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import DateIcon from '../icons/date-icon';
+import Button from '../ui/button';
 
 import classes from './event-item.module.css';
 
@@ -22,14 +25,21 @@ export default function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
