1675e4076751adcf088b9bb9126a77d96e1d1c3c
Dan Poulsen <dangjogvara@gmail.com>
2023-01-06 09:43:22 +0000

    added css

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import classes from './event-item.module.css';
+
 export default function EventItem(props) {
   const { title, image, date, location, id } = props;
 
-  const humanReadableDate = new Date(date).toLocaleDateString('en-GB', {
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
     day: 'numeric',
     month: 'long',
     year: 'numeric',
@@ -14,19 +16,19 @@ export default function EventItem(props) {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li key={id}>
-      <Image src={'/' + image} alt={title} width={500} height={500} />
-      <div>
-        <div>
+    <li className={classes.item}>
+      <Image src={'/' + image} alt={title} width={300} height={400} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
