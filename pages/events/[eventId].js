9bd9ebd322ff30b3b6f0ad643d1eef8cf7bc7f02
Dan Poulsen <dangjogvara@gmail.com>
2023-01-09 23:19:52 +0000

    section 4 finished

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -5,6 +5,7 @@ import { getEventById } from '../../dummy-data';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
 
 export default function EventDetailPage() {
   const router = useRouter();
@@ -13,7 +14,11 @@ export default function EventDetailPage() {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No Event found!</p>;
+    return (
+      <ErrorAlert>
+        <p>No Event found!</p>
+      </ErrorAlert>
+    );
   }
 
   return (
